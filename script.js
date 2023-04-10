60aa75c386ececc827289c0ab7d81291834be68b
rhysgreaney <126647880+rhysgreaney@users.noreply.github.com>
2023-04-10 17:44:06 +0800

    updated indenting

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ var addTaskButton = document.getElementById("add-task");
 var newTaskInput = document.getElementById("task-input");
 var todoListContainer = document.getElementById("todo-list");
 var templateContainer = document.getElementById("list-item-template");
-var template = templateContainer.innerHTML;
+var template = templateContainer.innerHTML
 
 // Step 2 Write the behaviour
 
 function onAddTaskClicked(event) {
-var taskName = newTaskInput.value;
-newTaskInput.value = "";
-var taskHTML = template.replace("<!--TASK NAME-->", taskName);
-todoListContainer.insertAdjacentHTML('afterbegin', taskHTML);
+    var taskName = newTaskInput.value;
+    newTaskInput.value = "";
+    var taskHTML = template.replace("<!--TASK NAME-->", taskName);
+    todoListContainer.insertAdjacentHTML('afterbegin', taskHTML);
 }
 
 function onTodoListContainerClicked(event){
+    var targetElement = event.toElement;
+
     while (!targetElement.classList.contains("task")){
-    targetElement = targetElement.parentalElement;
+        targetElement = targetElement.parentalElement;
     }
+
     var checkbox = targetElement.querySelector(".checkbox");
     if (checkbox.checked){
         targetElement.classList.add("completed")
